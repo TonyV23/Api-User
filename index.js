5c9f5b86df6335c46ef3a2a7d13d8042d2f920f2
viper23 <htonnycarlos@gmail.com>
2021-12-27 01:35:56 +0200

    finish to add function that will display user from db

--- index.js
+++ index.js
@@ -11,3 +11,31 @@ const fetchUser = async () =>{
     console.log(userData);    
 };
 
+// function that diplays user from database
+const displayUser = async () =>{
+    await fetchUser();
+
+    //function that will parse date format
+    const dataParser = (date) =>{
+        let newDate = new Date(date).toLocaleDateString("en-US",{
+            year : "numeric",
+            month : "long",
+            day : "numeric"
+        })
+        return newDate;
+    };
+
+    document.body.innerHTML = userData.map((user) =>
+        `
+        <div class ="card">
+            <img src =${user.picture.large} alt ="photo of ${user.name.first}">
+            <h3>${user.name.first}</h3>
+            <p>${user.location.city}, ${dataParser(user.dob.date)}</p>
+            <em>Member since ${user.registered.date} days ago</em>
+        </div>
+        
+        `
+    ).join("");
+};
+
+displayUser();
